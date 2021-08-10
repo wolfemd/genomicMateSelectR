ad606985ce73c0f9754c12fdafa9af482998eb38
wolfemd <wolfemd@gmail.com>
2021-08-10 11:18:43 -0400

    Remove the Dependencies. Return them to Imports. Correctly use Roxygen directives in R/genomicMateSelectR-package.R to create NAMESPACE.

--- R/genomicMateSelectR-package.R
+++ R/genomicMateSelectR-package.R
@@ -1,13 +1,12 @@
 #' @keywords internal
 "_PACKAGE"
 
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
 #' @import dplyr
 #' @import tidyr
 #' @import purrr
 #' @import tibble
-
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
-## usethis namespace: start
 ## usethis namespace: end
 NULL
